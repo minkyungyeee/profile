63083b3466594e528e374451ceae8a09af684a49
minkyungyeee <minkfree94@gmail.com>
2021-08-11 21:18:44 +0900

    Update

--- work/work0/js/work0.js
+++ work/work0/js/work0.js
@@ -1,8 +1,25 @@
 ;(function($){
     var work0 = {
         init:function(){
-
+            var that = this;
+                that.section2Fn();
         },
+        section2Fn:function(){
+            var winW = $(window).innerWidth();
+            var winH = $(window).innerHeight();
+            var $slide = $('#section2 .slide')
+
+            function resizeFn(){
+                winW = $(window).innerWidth();
+                $slide.css({width:winW});
+            }
+
+            setTimeout(resizeFn,10);
+
+            $(window).resize(function(){
+                setTimeout(resizeFn,10);
+            });
+        }
     }
 
     work0.init();
--- work/work1/js/work1.js
+++ work/work1/js/work1.js
@@ -1,9 +1,26 @@
 ;(function($){
-    var work1 = {
+    var work0 = {
         init:function(){
-
+            var that = this;
+                that.section2Fn();
         },
+        section2Fn:function(){
+            var winW = $(window).innerWidth();
+            var winH = $(window).innerHeight();
+            var $slide = $('#section2 .slide')
+
+            function resizeFn(){
+                winW = $(window).innerWidth();
+                $slide.css({width:winW});
+            }
+
+            setTimeout(resizeFn,10);
+
+            $(window).resize(function(){
+                setTimeout(resizeFn,10);
+            });
+        }
     }
 
-    work1.init();
+    work0.init();
 })(jQuery);
